d23e20fa6ba908d0902042deb92b3812f0f1df10
이성호 <dltjdgh0303@naver.com>
2024-01-23 18:33:47 +0900

    fix: ServerParam 클래스 롤백 및 타입 수정

--- srcs/param/server_param/ServerParam.cpp
+++ srcs/param/server_param/ServerParam.cpp
@@ -42,10 +42,42 @@ const std::string& ServerParam::getServerPassword() const {
   return this->serverPassword;
 }
 
-std::map<int, Client*> ServerParam::getClientMap() const {
-  return this->clientMap;
+void ServerParam::addClient(const int& clientFd, Client* client) {
+  assert(clientFd > 2);
+  assert(this->clientMap.find(clientFd) == this->clientMap.end());
+  this->clientMap.insert(std::make_pair(clientFd, client));
+  return;
 }
 
-std::map<std::string, Channel*> ServerParam::getChannelMap() const {
-  return this->channelMap;
+void ServerParam::addChannel(const std::string& channelName, Channel* channel) {
+  assert(channelName.empty() == false);
+  assert(this->channelMap.find(channelName) == this->channelMap.end());
+  this->channelMap.insert(std::make_pair(channelName, channel));
+  return;
+}
+
+void ServerParam::removeClient(const int& clientFd) {
+  assert(clientFd > 2);
+  assert(this->clientMap.find(clientFd) != this->clientMap.end());
+  this->clientMap.erase(clientFd);
+  return;
+}
+
+void ServerParam::removeChannel(const std::string& channelName) {
+  assert(channelName.empty() == false);
+  assert(this->channelMap.find(channelName) != this->channelMap.end());
+  this->channelMap.erase(channelName);
+  return;
+}
+
+Client* ServerParam::getClient(const int& clientFd) const {
+  assert(clientFd > 2);
+  assert(this->clientMap.find(clientFd) != this->clientMap.end());
+  return this->clientMap.find(clientFd)->second;
+}
+
+Channel* ServerParam::getChannel(const std::string& channelName) const {
+  assert(channelName.empty() == false);
+  assert(this->channelMap.find(channelName) != this->channelMap.end());
+  return this->channelMap.find(channelName)->second;
 }
