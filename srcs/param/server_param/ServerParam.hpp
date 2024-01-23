e3f55724b2e0ed924ede5299d8f258798120caee
이성호 <dltjdgh0303@naver.com>
2024-01-23 17:30:31 +0900

    fix: 이전 함수 삭제 및 새로운 함수 추가
    - 필요한 동작만 하도록 뒀던 함수는 현 상황에서 많은 것을 생각해야하기에 map을 주고 원하는 동작을 알아서 하도록 함
      - getClientMap() 추가
      - getChannelMap() 추가
      - addClient() 삭제
      - addChannel() 삭제
      - removeClient() 삭제
      - removeChannel() 삭제
      - getClient() 삭제
      - getChannel() 삭제

--- srcs/param/server_param/ServerParam.hpp
+++ srcs/param/server_param/ServerParam.hpp
@@ -29,12 +29,8 @@ class ServerParam {
   void setServerPassword(const std::string& serverPassword);
   const int& getServerFd() const;
   const std::string& getServerPassword() const;
-  bool addClient(const int& clientFd, Client* client);
-  bool addChannel(const std::string& channelName, Channel* channel);
-  bool removeClient(const int& clientFd);
-  bool removeChannel(const std::string& channelName);
-  Client* getClient(const int& clientFd) const;
-  Channel* getChannel(const std::string& channelName) const;
+  std::map<int, Client*> getClientMap() const;
+  std::map<std::string, Channel*> getChannelMap() const;
 };
 
 #endif
