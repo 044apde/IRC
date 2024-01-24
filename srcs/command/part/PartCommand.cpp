850afc5af16c23ea049a477db7194056834bb7cb
이성호 <dltjdgh0303@naver.com>
2024-01-24 19:15:37 +0900

    feat: PartCommand 클래스 구현

---
+++ srcs/command/part/PartCommand.cpp
@@ -0,0 +1,41 @@
+#include "PartCommand.hpp"
+
+PartCommand::PartCommand() : ACommand() {}
+
+PartCommand::~PartCommand() {}
+
+PartCommand::PartCommand(const PartCommand &other) { static_cast<void>(other); }
+
+PartCommand &PartCommand::operator=(const PartCommand &other) {
+  static_cast<void>(other);
+  return *this;
+}
+
+CommandResponseParam PartCommand::execute(ServerParam &serverParam,
+                                          ParsedParam &parsedParam) {
+  CommandResponseParam commandResponse;
+  int senderSocketFd = parsedParam.getSenderSocketFd();
+  std::string channelName = parsedParam.getChannel();
+  Channel *channel = serverParam.getChannel(channelName);
+  Client *client = serverParam.getClient(senderSocketFd);
+
+  if (parsedParam.getChannel().empty() == true) {
+    commandResponse.setResponseMessage(
+        this->replyMessage.errNeedMoreParams(parsedParam));
+  } else if (channel == NULL) {
+    commandResponse.setResponseMessage(
+        this->replyMessage.errNoSuchChannel(parsedParam));
+  } else if (client == NULL || channel->isClientInChannel(client) == false) {
+    commandResponse.setResponseMessage(
+        this->replyMessage.errNotOnChannel(parsedParam));
+  } else {
+    channel->setAllClientFd(commandResponse.getTargetClientFdSet());
+    commandResponse.setResponseMessage(
+        this->replyMessage.successPart(parsedParam));
+    serverParam.removeClientAndChannelEachOther(client, channel);
+  }
+  if (commandResponse.getTargetClientFdSet().empty() == true) {
+    commandResponse.addTargetClientFd(senderSocketFd);
+  }
+  return commandResponse;
+}
