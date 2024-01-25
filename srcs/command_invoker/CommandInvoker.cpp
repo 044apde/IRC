65981535e318f0267c0f76788f6baa761da176f6
이성호 <dltjdgh0303@naver.com>
2024-01-25 16:27:21 +0900

    feat: CommandInvoker 클래스 구현

--- srcs/command_invoker/CommandInvoker.cpp
+++ srcs/command_invoker/CommandInvoker.cpp
@@ -1,11 +1,19 @@
 #include "CommandInvoker.hpp"
 
 CommandInvoker::CommandInvoker() : replyMessage(ReplyMessage::getInstance()) {
-  this->commandMap.insert(std::make_pair("PASS", new PassCommand()));
-  this->commandMap.insert(std::make_pair("NICK", new NickCommand()));
-  this->commandMap.insert(std::make_pair("USER", new UserCommand()));
-  this->commandMap.insert(std::make_pair("JOIN", new JoinCommand()));
   this->commandMap.insert(std::make_pair("INVITE", new InviteCommand()));
+  this->commandMap.insert(std::make_pair("JOIN", new JoinCommand()));
+  this->commandMap.insert(std::make_pair("KICK", new KickCommand()));
+  this->commandMap.insert(std::make_pair("MODE", new ModeCommand()));
+  this->commandMap.insert(std::make_pair("NICK", new NickCommand()));
+  this->commandMap.insert(std::make_pair("PART", new PartCommand()));
+  this->commandMap.insert(std::make_pair("PASS", new PassCommand()));
+  this->commandMap.insert(std::make_pair("PING", new PingCommand()));
+  this->commandMap.insert(std::make_pair("PONG", new PongCommand()));
+  this->commandMap.insert(std::make_pair("PRIVMSG", new PrivmsgCommand()));
+  this->commandMap.insert(std::make_pair("QUIT", new QuitCommand()));
+  this->commandMap.insert(std::make_pair("TOPIC", new TopicCommand()));
+  this->commandMap.insert(std::make_pair("USER", new UserCommand()));
   return;
 }
 
@@ -18,14 +26,21 @@ CommandInvoker::~CommandInvoker() {
 }
 
 CommandResponseParam CommandInvoker::execute(ServerParam& serverParam,
-                                             ParsedParam& parsedParam) {
+                                             TokenParam& tokenParam) {
   std::map<std::string, ACommand*>::iterator it =
-      this->commandMap.find(parsedParam.getCommand());
+      this->commandMap.find(tokenParam.getCommand());
   if (it == this->commandMap.end()) {
     CommandResponseParam commandResponse;
+    ParsedParam parsedParam =
+        ParsedParamBuilder()
+            .senderSocketFd(tokenParam.getSenderSocketFd())
+            .command(tokenParam.getCommand())
+            .build();
+
     commandResponse.setResponseMessage(
         this->replyMessage.errUnknownCommand(parsedParam));
+    commandResponse.addTargetClientFd(tokenParam.getSenderSocketFd());
     return commandResponse;
   }
-  return it->second->execute(serverParam, parsedParam);
+  return it->second->execute(serverParam, tokenParam);
 }
