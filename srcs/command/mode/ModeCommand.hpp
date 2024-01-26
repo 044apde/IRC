c5395791aec98839406479e2c30f12713fe55b34
이성호 <dltjdgh0303@naver.com>
2024-01-27 04:27:11 +0900

    feat: ModeCommand 클래스 구현

--- srcs/command/mode/ModeCommand.hpp
+++ srcs/command/mode/ModeCommand.hpp
@@ -2,25 +2,38 @@
 #ifndef MODE_COMMAND_HPP
 #define MODE_COMMAND_HPP
 
+#include <cctype>
 #include <string>
+#include <vector>
 
 #include "../ACommand.hpp"
 #include "../channel/Channel.hpp"
 #include "../client/Client.hpp"
 #include "../param/command_response_param/CommandResponseParam.hpp"
-#include "../param/parsed_param/ParsedParam.hpp"
 #include "../param/server_param/ServerParam.hpp"
+#include "../param/token_param/TokenParam.hpp"
 
 class ModeCommand : public ACommand {
  private:
   ModeCommand(const ModeCommand& other);
   ModeCommand& operator=(const ModeCommand& other);
+  bool isValidSignedChar(const std::string& modeString, const size_t& index);
+  bool isValidMode(const char& modeChar);
+  bool isValidModeString(const std::string& modeString);
+  bool isValidModeArgument(const std::vector<std::string>& parameter);
+  bool isValidParamter(CommandResponseParam& commandResponse,
+                       const TokenParam& tokenParam);
+  bool IsDigitMaxUser(const std::string& argument);
+  size_t getDigitMaxUser(const std::string& argument);
+  bool executeMode(Client* client, Channel* channel,
+                   const std::string& modeString,
+                   std::vector<std::string> arguments);
 
  public:
   ModeCommand();
   ~ModeCommand();
   CommandResponseParam execute(ServerParam& serverParam,
-                               TokenParam& tokenParam);
+                               const TokenParam& tokenParam);
 };
 
 #endif
