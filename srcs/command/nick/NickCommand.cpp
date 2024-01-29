9e1133e1d260ed4bf52b28ca01f4dd66d402ee60
이성호 <dltjdgh0303@naver.com>
2024-01-29 18:02:48 +0900

    fix: NickCommand 클래스 수정

--- srcs/command/nick/NickCommand.cpp
+++ srcs/command/nick/NickCommand.cpp
@@ -11,28 +11,6 @@ NickCommand& NickCommand::operator=(const NickCommand& other) {
   return *this;
 }
 
-bool NickCommand::isValidNickname(const std::string& nickname) const {
-  if (nickname.empty() == true || nickname[0] == '#' || nickname[0] == '&' ||
-      nickname[0] == ':' || nickname[0] == ' ' ||
-      std::isdigit(nickname[0]) == true || nickname.size() > 9) {
-    return false;
-  }
-  return true;
-}
-
-bool NickCommand::isSameStrWithLowercase(const std::string& str1,
-                                         const std::string& str2) const {
-  if (str1.size() != str2.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < str1.size(); ++i) {
-    if (std::tolower(str1[i]) != std::tolower(str2[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
 bool NickCommand::isValidParamter(CommandResponseParam& commandResponse,
                                   const TokenParam& tokenParam) {
   const std::vector<std::string>& parameter = tokenParam.getParameter();
@@ -65,7 +43,7 @@ CommandResponseParam NickCommand::execute(ServerParam& serverParam,
   const std::string prevNickname = senderClient->getNickname();
   const std::string newNickname = parameter[0];
 
-  if (isValidNickname(newNickname) == true) {
+  if (isValidNickname(newNickname) == false) {
     commandResponse.addResponseMessage(
         senderSocketFd,
         this->replyMessage.errErroneusNickname("", newNickname));
--- srcs/command/nick/NickCommand.hpp
+++ srcs/command/nick/NickCommand.hpp
@@ -7,17 +7,15 @@
 #include <vector>
 
 #include "../../client/Client.hpp"
+#include "../../param/command_response_param/CommandResponseParam.hpp"
+#include "../../param/server_param/ServerParam.hpp"
+#include "../../param/token_param/TokenParam.hpp"
 #include "../ACommand.hpp"
-#include "../param/command_response_param/CommandResponseParam.hpp"
-#include "../param/server_param/ServerParam.hpp"
 
 class NickCommand : public ACommand {
  private:
   NickCommand(const NickCommand &other);
   NickCommand &operator=(const NickCommand &other);
-  bool isValidNickname(const std::string &nickname) const;
-  bool isSameStrWithLowercase(const std::string &str1,
-                              const std::string &str2) const;
   bool isValidParamter(CommandResponseParam &commandResponse,
                        const TokenParam &tokenParam);
 
